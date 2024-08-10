79cd76e1b10c81b594d310a7470993f776012f02
suzuyu0115 <szkyut0115@gmail.com>
2024-08-11 01:08:38 +0900

    31. ブログ本文の文字数制限を設けてみよう

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
 import { Article } from '../../types';
+import ArticleCard from './ArticleCard';
 
 type ArticleListProps = {
   articles: Article[];
@@ -11,29 +12,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
       {articles.map((article) => (
-        <article className='shadow my-4 flex flex-col' key={article.id}>
-          <Link href={`articles/${article.id}`} className='hover:opacity-75'>
-            <Image
-              src='/sample.png'
-              alt=''
-              width={1280}
-              height={300}
-            />
-          </Link>
-          <div className='bg-white flex flex-col justify-start p-6'>
-            <Link href={`articles/${article.id}`} className='text-blue-700 pb-4 font-bold'>
-              Technology
-            </Link>
-            <Link href={`articles/${article.id}`} className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'>
-              {article.title}
-            </Link>
-            <p className='text-sm pb-3 text-slate-900'>published on {article.createdAt}</p>
-            <a href={`articles/${article.id}`} className='text-slate-900 pb-6'>
-              {article.content}
-            </a>
-            <Link href={`articles/${article.id}`} className='text-pink-800 hover:text-black'>続きを読む</Link>
-          </div>
-        </article>
+        <ArticleCard article={article} key={article.id} />
       ))}
     </div>
   )
