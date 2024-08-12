2a5780ca879cbffd9b6df78d5ac800bcd8a404fb
suzuyu0115 <szkyut0115@gmail.com>
2024-08-13 00:48:33 +0900

    37. ブログ記事削除用のAPIを作ってみよう

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,9 +1,12 @@
+import DeleteButton from '@/app/components/DeleteButton';
 import { getDetailArticle } from '@/blogAPI';
 import Image from 'next/image';
 import React from 'react';
 
 const Article = async ({ params }: { params: { id: string } }) => {
   const detailArticle = await getDetailArticle(params.id);
+
+  const handleDelete = () => {}
 
   return (
     <div className='max-w-3xl mx-auto p-5'>
@@ -18,6 +21,9 @@ const Article = async ({ params }: { params: { id: string } }) => {
       </h1>
       <div className='text-lg leading-relaxed text-justify'>
         <p>{detailArticle.content}</p>
+      </div>
+      <div className='text-right mt-3'>
+        <DeleteButton id={detailArticle.id} />
       </div>
     </div>
   );
---
+++ src/app/components/DeleteButton.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react';
+
+const DeleteButton = () => {
+  return (
+    <div>DeleteButton</div>
+  );
+};
+
+export default DeleteButton;
