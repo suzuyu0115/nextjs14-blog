cf9f6786b0942b888b9f81ba8e99617c2a87e547
suzuyu0115 <szkyut0115@gmail.com>
2024-08-13 00:58:08 +0900

    38. 削除ボタンを作って実際にAPIを叩いて削除してみよう

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -6,8 +6,6 @@ import React from 'react';
 const Article = async ({ params }: { params: { id: string } }) => {
   const detailArticle = await getDetailArticle(params.id);
 
-  const handleDelete = () => {}
-
   return (
     <div className='max-w-3xl mx-auto p-5'>
       <Image
--- src/app/components/DeleteButton.tsx
+++ src/app/components/DeleteButton.tsx
@@ -1,10 +1,28 @@
 'use client'
 
+import { deleteArticle } from '@/blogAPI';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
-const DeleteButton = () => {
+type DeleteButtonProps = {
+  id: string;
+};
+
+const DeleteButton = ({ id }: DeleteButtonProps) => {
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    await deleteArticle(id);
+
+    router.push("/");
+    router.refresh();
+  }
+
   return (
-    <div>DeleteButton</div>
+    <div
+      className='bg-red-500 hover:bg-red-600 rounded-md py-2 px-5 inline cursor-pointer'
+      onClick={handleDelete}
+    >削除</div>
   );
 };
 
