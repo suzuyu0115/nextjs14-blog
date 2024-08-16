6cd2c9728a883e07302b9306422868bf882cebf5
suzuyu0115 <szkyut0115@gmail.com>
2024-08-17 01:19:15 +0900

    47. app/apiディレクトリに簡易APIを作ってみよう

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,17 +1,17 @@
-import { supabase } from "@/utils/supabaseClient";
-import { NextApiRequest, NextApiResponse } from "next";
+// import { supabase } from "@/utils/supabaseClient";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { data, error } = await supabase.from("posts").select("*");
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const { data, error } = await supabase.from("posts").select("*");
 
-  console.log(data);
+//   console.log(data);
 
-  if (error) {
-    return res.status(500).json({ error: error.message });
-  }
+//   if (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
 
-  return res.status(200).json(data);
-}
+//   return res.status(200).json(data);
+// }
